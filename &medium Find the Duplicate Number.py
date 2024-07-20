878d3a0fe2c40252fc4d7cf297590e2b225a0cb5
noahk0 <noah.kim@mail.utoronto.ca>
2024-07-20 19:08:48 +0900

    Create &medium Find the Duplicate Number.py

---
+++ &medium Find the Duplicate Number.py
@@ -0,0 +1,12 @@
+def findDuplicate(self, nums: List[int]) -> int:
+    slow, fast = nums[0], nums[nums[0]]
+
+    while slow != fast:
+        slow, fast = nums[slow], nums[nums[fast]]
+
+    slow = 0
+
+    while slow != fast:
+        slow, fast = nums[slow], nums[fast]
+
+    return slow
