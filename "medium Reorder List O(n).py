5aff0440c2f9581bd0f6ad49eff2a3a2c4ff9cb7
noahk0 <noah.kim@mail.utoronto.ca>
2024-07-27 17:18:15 +0900

    Update "medium Reorder List O(n).py

--- "medium Reorder List O(n).py
+++ "medium Reorder List O(n).py
@@ -2,20 +2,14 @@ def reorderList(self, head: Optional[ListNode]) -> None:
     fast = slow = head
 
     while fast and fast.next:
-        slow = slow.next
-        fast = fast.next.next
+        slow, fast = slow.next, fast.next.next
 
     pre = None
         
     while slow:
         cur = slow.next
-        slow.next = pre
-        pre = slow
-        slow = cur
+        slow.next, pre, slow = pre, slow, cur
 
     while pre.next:
         tmp = head.next
-        head.next = pre
-        pre = pre.next
-        head.next.next = tmp
-        head = tmp
+        head.next, pre, head.next.next, head = pre, pre.next, tmp, tmp
