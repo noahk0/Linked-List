7b8aedf7214c6663e15ca78b8523cfb3c9e92f26
noahk0 <noah.kim@mail.utoronto.ca>
2024-07-27 17:22:46 +0900

    Update %medium Add Two Numbers O(max(n, m)).py

--- %medium Add Two Numbers O(max(n, m)).py
+++ %medium Add Two Numbers O(max(n, m)).py
@@ -7,11 +7,7 @@ def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optio
         add += l1.val if l1 else 0
         add += l2.val if l2 else 0
 
-        num.val = add % 10
-        num.next = ListNode(add // 10) if add // 10 or l1 and l1.next or l2 and l2.next else None
-
-        num = num.next
-        l1 = l1.next if l1 else None
-        l2 = l2.next if l2 else None
+        num.val, num.next = add % 10, ListNode(add // 10) if add // 10 or l1 and l1.next or l2 and l2.next else None
+        num, l1, l2 = num.next, l1.next if l1 else None, l2.next if l2 else None
 
     return head
