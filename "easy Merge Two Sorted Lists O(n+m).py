c167cf35e1950dbd91bca5a3e567d1be7afe29e5
noahk0 <noah.kim@mail.utoronto.ca>
2024-07-27 17:15:45 +0900

    Update "easy Merge Two Sorted Lists O(n+m).py

--- "easy Merge Two Sorted Lists O(n+m).py
+++ "easy Merge Two Sorted Lists O(n+m).py
@@ -3,11 +3,9 @@ def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) ->
 
     while list1 and list2:
         if list1.val < list2.val:
-            cur.next = list1
-            list1 = list1.next
+            cur.next, list1 = list1, list1.next
         else:
-            cur.next = list2
-            list2 = list2.next
+            cur.next, list2 = list2, list2.next
 
         cur = cur.next
 
